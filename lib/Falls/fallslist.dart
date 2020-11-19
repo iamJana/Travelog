50b28c168816c63e8b65f15949a251ccb5006bb6
jana <janamurugesan01@gmail.com>
2020-11-19 10:23:57 +0530

    Falls Half work done

--- lib/Falls/fallslist.dart
+++ lib/Falls/fallslist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'description.dart';
 
 class Falllist extends StatelessWidget {
   final String _path;
@@ -6,12 +7,106 @@ class Falllist extends StatelessWidget {
   final String _name;
   final String _subname;
   Falllist(this._path, this._name, this._subname, this._count);
+  // ignore: missing_return
+  Widget decide(_count) {
+    if (_count == 0) {
+      return Detail(
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+      );
+    } else if (_count == 1) {
+      return Detail(
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+      );
+    } else if (_count == 2) {
+      return Detail(
+         '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',);
+    } else if (_count == 3) {
+      return Detail(
+          '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',);
+    } else if (_count == 4) {
+      return Detail(
+          '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',);
+    } else if (_count == 5) {
+      return Detail(
+          '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',);
+    } else if (_count == 6) {
+      return Detail(
+         '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',);
+    }
+    else if (_count == 7) {
+      return Detail(
+         '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => decide(_count),
+            ),
+          );
+        },
         child: Container(
           height: 80.0,
           child: Center(
@@ -22,7 +117,7 @@ class Falllist extends StatelessWidget {
               ),
               trailing: Icon(Icons.arrow_forward_ios),
               title: Text(
-               _name,
+                _name,
                 style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
               ),
               subtitle: Text(
